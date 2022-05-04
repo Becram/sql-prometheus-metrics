60bcd3419392993f54adbe4b659d00d9b949a859
Becram <bikram.dhoju@gmail.com>
2022-05-04 14:45:56 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Becram/sql-prometheus-metrics/pkg/home"
+	"github.com/Becram/sql-prometheus-metrics/pkg/k8s"
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		home.HomeHandler,
+	},
+	Route{
+		"restartDeployment",
+		"POST",
+		"/restart",
+		k8s.RestartDeployment,
+	},
+	Route{
+		"listDeployment",
+		"POST",
+		"/list",
+		k8s.ListDeployment,
+	},
+}
+
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func main() {
+
+	router := NewRouter()
+	fmt.Print("Serving http request at localhost:8080.....\n")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
